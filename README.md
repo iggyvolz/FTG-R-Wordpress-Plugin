bcaf780a9f3c103ac4a024b7bbbe3ea294189660
iggyvolz <iggyvolz@aim.com>
2013-12-06 17:21:35 -0500

    Initial commit.

--- README.md
+++ README.md
@@ -2,3 +2,6 @@ FTG-R-Wordpress-Plugin
 ======================
 
 Wordpress plugin for Fightmon the Game: Reemon
+
+Put this under your wordpress/wp-content/wp-plugins folder to allow for a FTG:R dashboard widget.  Note that you must also have FTG:R installed on your web server and edit the fightmon-the-game-reemon.php file for the path to it (we will include FTG:R in a future version)
+To obtain a copy of Fightmon the Game: Reemon, please see the <a href="https://github.com/iggyvolz/Fightmon-the-Game--Reemon">GitHub repository</a>.
---
+++ fightmon-the-game-reemon.php
@@ -0,0 +1,35 @@
+<?php
+
+/*
+  Plugin Name: Fightmon the Game: Reemon Wordpress Linker
+  Plugin URI: https://github.com/iggyvolz/FTG-R-Wordpress-Plugin
+  Description: Link from Fightmon the Game: Reemon to Wordpress to allow for a Wordpress Dashboard Widget of Fightmon the Game: Reemon.  Note that you must also have Fightmon the Game: Reemon installed on the server.
+  Author: iggyvolz
+  Version: 0.3
+  Author URI: http://fightmon.eternityincurakai.com/
+ */
+define('FTGR_BASE', 'http://localhost/ftgr-dev'); // Put the absolute URL of Fightmon the Game: Reemon.
+
+function add_ftgr_base_function()
+{
+
+	wp_add_dashboard_widget(
+			'fightmon_the_game_reemon', // Widget slug.
+			'Fightmon the Game: Reemon', // Title.
+			'add_ftgr_function' // Display function.
+	);
+}
+
+add_action('wp_dashboard_setup', 'add_ftgr_base_function');
+
+function add_ftgr_function()
+{
+	if (!defined('FTGR_BASE'))
+	{
+		echo 'FTGR_BASE is not defined';
+		return;
+	}
+	echo '<iframe src="' . FTGR_BASE . '" style="height:400px; width:500px">';
+}
+
+?>
